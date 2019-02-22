d63930a8a1ae2eddb6bfb4886f32002dd2eee4c9
陈才龙 <675687771@qq.com>
2019-02-22 15:13:53 +0800

    备份

--- page-action-demo/js/background.js
+++ page-action-demo/js/background.js
@@ -1,18 +1,41 @@
-chrome.runtime.onInstalled.addListener(function(ex){
-	console.log('这里是QQ图标的',this)
+chrome.runtime.onInstalled.addListener(function(ex){//程序第一次安装或更新时会执行此方法
+	console.info('这里是QQ图标的this',this)
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
-		chrome.declarativeContent.onPageChanged.addRules([
-			{
+		chrome.declarativeContent.onPageChanged.addRules([{
 				conditions: [
 					// 只有打开百度才显示pageAction
 					new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlContains: 'baidu.com'}})
 				],
 				actions: [new chrome.declarativeContent.ShowPageAction()]
-			}
-		]);
-	});
+		}])//添加规则调用
+	})//移除规则后调用
 })//这个是原版的
 chrome.runtime.getBackgroundPage(function (eee){
-	console.log(eee)
+	console.info('获取到当前的后台页面',eee)
 })//获取后台页面的window对象
 console.info('这个是获取清单文件详情',chrome.runtime.getManifest())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- simple-chrome-plugin-demo/content-script.js
+++ simple-chrome-plugin-demo/content-script.js
@@ -1,3 +1,5 @@
 ﻿(function() {
 	console.log('这是 simple-chrome-plugin-demo 的content-script！');
+
+
 })();
